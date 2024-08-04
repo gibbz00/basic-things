59344768747ae8544d6276a04fab03f1ef3038d1
gibbz00 <gabrielhansson00@gmail.com>
2024-08-04 10:01:02 +0200

    feat(cargo-generate): migrate `AUTHOR_EMAIL`

--- template.sh
+++ template.sh
@@ -5,7 +5,6 @@ replace_all() {
 
 # Assumptions: hosted on github.com, default branch is main.
 replace_all 'PROJECT_DESCRIPTION' 'Some description'
-replace_all 'AUTHOR_EMAIL' 'First Last <some@email.com>'
 replace_all 'GITHUB_PATH' 'username/repo'
 replace_all 'GITHUB_PAGES_URL' 'username.github.io'
 replace_all 'CODE_COV_TOKEN' 'xxx'
