7e79be5bf7a34a8a3ea5d7fadace3f691c7a4b14
gibbz00 <gabrielhansson00@gmail.com>
2024-08-04 16:06:09 +0200

    feat: optional book template creation

--- template.sh
+++ template.sh
@@ -2,6 +2,3 @@
 replace_all() {
   fd --type file . | xargs sed -i -e "s^$1^$2^g"
 }
-
-# Assumptions: hosted on github.com, default branch is main.
-replace_all 'GITHUB_PAGES_URL' 'username.github.io'
